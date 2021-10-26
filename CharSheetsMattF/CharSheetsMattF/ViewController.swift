12293c26d4a78b585c3829db5bb31346db3e5c5e
Matthew <frank.futchko@gmail.com>
2021-10-26 13:38:31 -0500

    oh dear god

--- CharSheetsMattF/CharSheetsMattF/ViewController.swift
+++ CharSheetsMattF/CharSheetsMattF/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var charName: UITextField!
+    @IBOutlet weak var levelSelect: UIStepper!
+    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var race: UIPickerView!
+    @IBOutlet weak var mainClass: UIPickerView!
+    @IBOutlet weak var subclass: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
