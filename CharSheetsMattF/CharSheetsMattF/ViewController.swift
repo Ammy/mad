f3a795d04be4f5708f4bc519b56109c30a5f3c3a
matt <futchko.matt@gmail.com>
2021-11-02 13:44:32 -0500

    huh

--- CharSheetsMattF/CharSheetsMattF/ViewController.swift
+++ CharSheetsMattF/CharSheetsMattF/ViewController.swift
@@ -16,13 +16,54 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return 1
     }
     
-
+    var noProfColor = UIColor.systemBlue
+    var profColor = UIColor.black
     @IBOutlet weak var charName: UITextField!
     @IBOutlet weak var levelSelect: UIStepper!
     @IBOutlet weak var level: UILabel!
     @IBOutlet weak var race: UIPickerView!
     @IBOutlet weak var mainClass: UIPickerView!
     @IBOutlet weak var subclass: UIPickerView!
+    @IBOutlet weak var expertise: UISegmentedControl!
+    @IBAction func expertiseCheck(_ sender: Any) {
+    switch expertise.selectedSegmentIndex
+            {
+            case 0:
+                profColor = UIColor.black
+                return
+            case 1:
+               profColor = UIColor.systemRed
+                return
+            default:
+                break
+            }
+    }
+    @IBOutlet weak var skill1: UIButton!
+    @IBOutlet weak var skill2: UIButton!
+    @IBOutlet weak var skill3: UIButton!
+    @IBOutlet weak var skill4: UIButton!
+    var button = [UIButton]()
+    @IBAction func changeLanguage(sender: UIButton) {
+        button = [skill1, skill2, skill3, skill4]
+        for n in 0...3 {
+        
+        switch sender {
+        
+        case button[n]:
+            if button[n].titleColor(for: .normal) == profColor {
+                button[n].setTitleColor(noProfColor, for: .normal)
+            }else{
+                button[n].setTitleColor(profColor, for: .normal)}
+
+        default:
+            
+            print("Unknown button")
+            
+        }}
+    }
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
