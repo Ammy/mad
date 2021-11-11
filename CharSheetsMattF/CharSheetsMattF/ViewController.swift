47bba27a8fbe546017620c0a1e912cc88c564f2c
matt <futchko.matt@gmail.com>
2021-11-11 13:48:16 -0600

    dang

--- CharSheetsMattF/CharSheetsMattF/ViewController.swift
+++ CharSheetsMattF/CharSheetsMattF/ViewController.swift
@@ -6,15 +6,9 @@
 //
 
 import UIKit
-
+import Foundation
+import CoreData
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 1
-    }
     
     var noProfColor = UIColor.systemBlue
     var profColor = UIColor.black
@@ -104,9 +98,52 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var tool46: UIButton!
     @IBOutlet weak var tool47: UIButton!
     @IBOutlet weak var tool48: UIButton!
-    
-    
+    @IBOutlet weak var lang1: UIButton!
+    @IBOutlet weak var lang2: UIButton!
+    @IBOutlet weak var lang3: UIButton!
+    @IBOutlet weak var lang4: UIButton!
+    @IBOutlet weak var lang5: UIButton!
+    @IBOutlet weak var lang6: UIButton!
+    @IBOutlet weak var lang7: UIButton!
+    @IBOutlet weak var lang8: UIButton!
+    @IBOutlet weak var lang9: UIButton!
+    @IBOutlet weak var lang10: UIButton!
+    @IBOutlet weak var lang11: UIButton!
+    @IBOutlet weak var lang12: UIButton!
+    @IBOutlet weak var lang13: UIButton!
+    @IBOutlet weak var lang14: UIButton!
+    @IBOutlet weak var lang15: UIButton!
+    var dataManager: NSManagedObjectContext!
+    var listArray = [NSManagedObject]()
+    @IBAction func saveButton(_ sender: Any) {
+        let newEntity = NSEntityDescription.insertNewObject(forEntityName: "Character", into: dataManager)
+        button = [skill1, skill2, skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16, skill17, skill18, tool1, tool2, tool3, tool4, tool5, tool6, tool7, tool8, tool9, tool10, tool11, tool12, tool13, tool14, tool15, tool16, tool17, tool18, tool19, tool20, tool21, tool22, tool23, tool24, tool25, tool26, tool27, tool28, tool29, tool30, tool31, tool32, tool33, tool34, tool35, tool36, tool37, tool38, tool39, tool40, tool41, tool42, tool43, tool44, tool45, tool46, tool47, tool48]
+        var st = ""
+        for n in 0...65 {
+            if button[n].titleColor(for: .normal) == UIColor.black {
+                st = "1"+st
+            }else if button[n].titleColor(for: .normal) == UIColor.systemRed{
+               st = "2"+st
+            }else{
+                st="0"+st
+            }
+            
+        }
+            print(st)
+newEntity.setValue( st, forKey: "profSTL")
+                do{
+                    try self.dataManager.save()
+                    listArray.append(newEntity)
+                }catch{
+                    print("Error Saving Data")
+                }
+                fetchData()
+            }
+
+        
+
     var button = [UIButton]()
+
     @IBAction func changeLanguage(sender: UIButton) {
         button = [skill1, skill2, skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16, skill17, skill18, tool1, tool2, tool3, tool4, tool5, tool6, tool7, tool8, tool9, tool10, tool11, tool12, tool13, tool14, tool15, tool16, tool17, tool18, tool19, tool20, tool21, tool22, tool23, tool24, tool25, tool26, tool27, tool28, tool29, tool30, tool31, tool32, tool33, tool34, tool35, tool36, tool37, tool38, tool39, tool40, tool41, tool42, tool43, tool44, tool45, tool46, tool47, tool48]
         for n in 0...65 {
@@ -114,29 +151,68 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         switch sender {
         
         case button[n]:
-            if button[n].titleColor(for: .normal) == profColor {
+            if button[n].titleColor(for: .normal) == profColor && n < 65 {
                 button[n].setTitleColor(noProfColor, for: .normal)
             }else{
                 button[n].setTitleColor(profColor, for: .normal)}
 
         default:
-            if n == 66  {
+            if n > 66 {
                 print("Unknown button")
                 return
 
-            }}
+            }
             
         }}
+    }
+    func fetchData(){
+        let fetchRequest : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Character")
+        do{
+            let result = try dataManager.fetch(fetchRequest)
+            listArray = result as! [NSManagedObject]
+            for item in listArray {
+                let product = item.value(forKey: "profSTL") as! String
+                
+            }
+            
+        }catch{
+                print("Error retriving data")
+            }
     
     
-    
-    
-    
+    }
+   
+    @IBOutlet weak var raceChoice: UIPickerView!
+    @IBOutlet weak var classChoice: UIPickerView!
+    @IBOutlet weak var subClassChoice: UIPickerView!
+    let races = ["Dragonborn","Dwarf","Elf","Gnome","Gnome","Half-Elf","Halfling","Half-Orc","Human","Tiefling"]
+    var classes:[String] = [String]()
+    var Data:[[String]] = [[String]]()
+     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return classes.count
+        }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return classes[component]}
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        classChoice.dataSource = self
+        classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+        classChoice.delegate = self
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        dataManager = appDelegate.persistentContainer.viewContext
+        fetchData()
+       
     }
-
-
 }
+
+
+
+
 
